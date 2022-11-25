633b629eeed47a2db3427fe3e1e287c8816483a7
rajeshveeru <rajeshveeru.cs@gmail.com>
2022-11-25 19:40:45 +0530

    component created

---
+++ dynamicSidenav/src/app/components/app-side-nav/app-side-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-side-nav',
+  templateUrl: './app-side-nav.component.html',
+  styleUrls: ['./app-side-nav.component.scss']
+})
+export class AppSideNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
